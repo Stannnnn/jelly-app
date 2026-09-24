535320343b70167d4f7f52de3dffa4a0f92692c2
Stannnnn <stann@live.nl>
2026-09-24 19:55:26 +0200

    added subsearch to searchparams

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,5 +1,6 @@
 import { HeartFillIcon } from '@primer/octicons-react'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { InlineLoader } from '../components/InlineLoader'
 import { DownloadIndicators, MediaList } from '../components/MediaList'
 import { PlaylistTrackList } from '../components/PlaylistTrackList'
@@ -22,8 +23,27 @@ export const Favorites = () => {
     const { isOpen, onContextMenu } = useDropdownContext()
     const { customItem: favoritesCustomItem } = useJellyfinCustomContainerItem('favorites', 'Favorite Songs')
 
-    const [searchQuery, setSearchQuery] = useState('')
+    const navigate = useNavigate()
+    const [searchQuery, setSearchQuery] = useState(new URLSearchParams(location.search).get('filter') || '')
     const { searchResults, searchLoading } = useJellyfinSearch(searchQuery)
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            const params = new URLSearchParams(location.search)
+            if (searchQuery) {
+                params.set('filter', searchQuery)
+            } else {
+                params.delete('filter')
+            }
+            const newSearch = params.toString()
+            const newUrl = newSearch ? `?${newSearch}` : location.pathname
+            const currentUrl = location.search || location.pathname
+            if (newUrl !== currentUrl) {
+                navigate(newUrl, { replace: true })
+            }
+        }, 200)
+        return () => clearTimeout(timer)
+    }, [searchQuery, navigate])
 
     const filteredTracks = searchQuery
         ? searchLoading && searchResults.length === 0
